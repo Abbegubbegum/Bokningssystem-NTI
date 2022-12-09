e2061ce52961473070a9c32bbf81dac12bb5dbff
Albin Norback <albin.nojback@gmail.com>
2022-12-09 14:13:41 +0100

    Add JSON Assertion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import { resolve } from "path";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.js";
 import admin from "firebase-admin";
-import serviceKey from "./serviceKey.json";
+import serviceKey from "./serviceKey.json" assert { type: "json" };
 
 const app = express();
 const PORT = process.env.PORT || 3000;
