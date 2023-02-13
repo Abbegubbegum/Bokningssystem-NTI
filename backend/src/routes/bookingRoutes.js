2c2945f21e7a90adedc345ae748e5b6480d783c1
Markus Piotrowski <Markuspiotrowski@live.com>
2023-02-13 10:41:47 +0100

    Added CreateBooking

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -26,19 +26,33 @@ router.post("/", authUser, async (req, res) => {
     return;
   }
 
-  bookingModel
-    .create({
-      booker: req.user._id,
-      room: req.body.roomNumber,
-      start: Date.parse(req.body.from),
-      end: req.body.to,
-    })
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
+  let bookings = await bookingModel.find({ roomNumber: req.body.roomNumber });
+
+  if (bookings.length != 0) {
+    bookings.forEach((booking) => {
+      if (
+        (booking.start >= req.body.from && booking.start >= req.body.to) ||
+        (booking.end <= req.body.from && booking.end <= req.body.to)
+      ) {
+        bookingModel
+          .create({
+            booker: req.user._id,
+            room: req.body.roomNumber,
+            start: Date.parse(req.body.from),
+            end: req.body.to,
+          })
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch((err) => {
+            res.status(400).send(err);
+          });
+      } else {
+        res.status(400).send("Time already booked!");
+        return;
+      }
     });
+  }
 });
 
 export default router;
