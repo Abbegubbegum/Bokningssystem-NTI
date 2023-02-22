f5a4629aed30aad15db5a3f086accfffba5b8615
Markus Piotrowski <Markuspiotrowski@live.com>
2023-02-22 14:11:25 +0100

    Add GetUsers

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,30 +4,40 @@ import userModel from "../models/user.js";
 
 const router = new Router();
 
-router.get("/", authAdmin, (req, res) => {});
+router.get("/", authAdmin, (req, res) => {
+  userModel
+    .find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
 
 router.post("/login", authEmail, async (req, res) => {
-	try {
-		const findResult = await userModel.findOne({
-			firebaseID: req.user.firebaseID,
-		});
+  try {
+    const findResult = await userModel.findOne({
+      firebaseID: req.user.firebaseID,
+    });
 
-		if (findResult) {
-			return res.status(200).json(findResult);
-		}
-	} catch {
-		return res.sendStatus(500);
-	}
+    if (findResult) {
+      return res.status(200).json(findResult);
+    }
+  } catch {
+    return res.sendStatus(500);
+  }
 
-	userModel
-		.create(req.user)
-		.then((result) => {
-			res.status(201).json(result);
-		})
-		.catch((err) => {
-			console.error(err);
-			res.sendStatus(500);
-		});
+  userModel
+    .create(req.user)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 export default router;
